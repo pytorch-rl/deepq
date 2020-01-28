c1b8913e83b3df1057dee6db86f281ca41bc5a40
amitka <amit.kan@samsung.com>
2020-01-28 11:00:04 -0600

    1. added validation for the algorithm -     a. generated validation set using validation_set_generator.py     b. added method trainer.validate()     c. gym_utils.get_screen() no longer moves to screen to device     d. added visualization.plot_validation_score()

--- config/default_config.py
+++ config/default_config.py
@@ -4,7 +4,9 @@ from yacs.config import CfgNode as CN
 def get_default_params():
     default_params = dict()
 
-    default_params['PATHS'] = {}
+    default_params['PATHS'] = {
+        'VALIDATION_SET_PATH': '/raid/algo/SOCISP_SLOW/ADAS/Courses/mspacman/data/validation_set_random_1000.pickle'
+    }
 
     default_params['TRAIN'] = {
         'BATCH_SIZE': 128,
@@ -13,6 +15,7 @@ def get_default_params():
         'EPS_START': 0.9,
         'EPS_END':  0.05,
         'EPS_DECAY': 200,
+        'VALIDATE_FREQUENCY': 10,
     }
 
     default_params['LOG'] = {}
